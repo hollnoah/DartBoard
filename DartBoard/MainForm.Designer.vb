bb88e0c9d550b60f93465c2ea525140d8917852e
Noah Holloway <noahholloway23@gmail.com>
2025-11-26 13:59:52 -0700

    Add project files.

---
+++ DartBoard/MainForm.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DartboardPictureBox = New System.Windows.Forms.PictureBox()
+        Me.StartRoundButton = New System.Windows.Forms.Button()
+        Me.ReviewButton = New System.Windows.Forms.Button()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.RoundsListBox = New System.Windows.Forms.ListBox()
+        Me.ModeLabel = New System.Windows.Forms.Label()
+        Me.RoundNumberLabel = New System.Windows.Forms.Label()
+        Me.ThrowCountLabel = New System.Windows.Forms.Label()
+        Me.MainToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.OpenLogButton = New System.Windows.Forms.Button()
+        CType(Me.DartboardPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DartboardPictureBox
+        '
+        Me.DartboardPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DartboardPictureBox.Location = New System.Drawing.Point(12, 12)
+        Me.DartboardPictureBox.Name = "DartboardPictureBox"
+        Me.DartboardPictureBox.Size = New System.Drawing.Size(400, 400)
+        Me.DartboardPictureBox.TabIndex = 0
+        Me.DartboardPictureBox.TabStop = False
+        '
+        'StartRoundButton
+        '
+        Me.StartRoundButton.Location = New System.Drawing.Point(430, 20)
+        Me.StartRoundButton.Name = "StartRoundButton"
+        Me.StartRoundButton.Size = New System.Drawing.Size(110, 30)
+        Me.StartRoundButton.TabIndex = 1
+        Me.StartRoundButton.Text = "&Start Round"
+        Me.StartRoundButton.UseVisualStyleBackColor = True
+        '
+        'ReviewButton
+        '
+        Me.ReviewButton.Location = New System.Drawing.Point(430, 60)
+        Me.ReviewButton.Name = "ReviewButton"
+        Me.ReviewButton.Size = New System.Drawing.Size(110, 30)
+        Me.ReviewButton.TabIndex = 2
+        Me.ReviewButton.Text = "&Review Mode"
+        Me.ReviewButton.UseVisualStyleBackColor = True
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(430, 100)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(110, 30)
+        Me.QuitButton.TabIndex = 3
+        Me.QuitButton.Text = "&Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'RoundsListBox
+        '
+        Me.RoundsListBox.FormattingEnabled = True
+        Me.RoundsListBox.ItemHeight = 16
+        Me.RoundsListBox.Location = New System.Drawing.Point(430, 169)
+        Me.RoundsListBox.Name = "RoundsListBox"
+        Me.RoundsListBox.Size = New System.Drawing.Size(350, 148)
+        Me.RoundsListBox.TabIndex = 4
+        '
+        'ModeLabel
+        '
+        Me.ModeLabel.AutoSize = True
+        Me.ModeLabel.Location = New System.Drawing.Point(427, 353)
+        Me.ModeLabel.Name = "ModeLabel"
+        Me.ModeLabel.Size = New System.Drawing.Size(75, 16)
+        Me.ModeLabel.TabIndex = 5
+        Me.ModeLabel.Text = "Mode: Play"
+        '
+        'RoundNumberLabel
+        '
+        Me.RoundNumberLabel.AutoSize = True
+        Me.RoundNumberLabel.Location = New System.Drawing.Point(427, 378)
+        Me.RoundNumberLabel.Name = "RoundNumberLabel"
+        Me.RoundNumberLabel.Size = New System.Drawing.Size(60, 16)
+        Me.RoundNumberLabel.TabIndex = 6
+        Me.RoundNumberLabel.Text = "Round: 0"
+        '
+        'ThrowCountLabel
+        '
+        Me.ThrowCountLabel.AutoSize = True
+        Me.ThrowCountLabel.Location = New System.Drawing.Point(427, 403)
+        Me.ThrowCountLabel.Name = "ThrowCountLabel"
+        Me.ThrowCountLabel.Size = New System.Drawing.Size(68, 16)
+        Me.ThrowCountLabel.TabIndex = 7
+        Me.ThrowCountLabel.Text = "Throw: 0/3"
+        '
+        'OpenLogButton
+        '
+        Me.OpenLogButton.Location = New System.Drawing.Point(587, 38)
+        Me.OpenLogButton.Name = "OpenLogButton"
+        Me.OpenLogButton.Size = New System.Drawing.Size(131, 59)
+        Me.OpenLogButton.TabIndex = 8
+        Me.OpenLogButton.Text = "Open Log (Notepad)"
+        Me.OpenLogButton.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.OpenLogButton)
+        Me.Controls.Add(Me.ThrowCountLabel)
+        Me.Controls.Add(Me.RoundNumberLabel)
+        Me.Controls.Add(Me.ModeLabel)
+        Me.Controls.Add(Me.RoundsListBox)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.ReviewButton)
+        Me.Controls.Add(Me.StartRoundButton)
+        Me.Controls.Add(Me.DartboardPictureBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dart Game Simulator"
+        CType(Me.DartboardPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DartboardPictureBox As PictureBox
+    Friend WithEvents StartRoundButton As Button
+    Friend WithEvents ReviewButton As Button
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents RoundsListBox As ListBox
+    Friend WithEvents ModeLabel As Label
+    Friend WithEvents RoundNumberLabel As Label
+    Friend WithEvents ThrowCountLabel As Label
+    Friend WithEvents MainToolTip As ToolTip
+    Friend WithEvents OpenLogButton As Button
+End Class
---
+++ DartBoard/MainForm.vb
@@ -0,0 +1,326 @@
+﻿'Noah Holloway
+'RCET 3371
+'Dart Board Game
+'
+Imports System.IO
+Imports System.Diagnostics
+Imports System.Linq
+
+Public Class MainForm
+
+    ' --- Constants / Config ---
+    Private Const NumThrowsPerRound As Integer = 3
+    Private ReadOnly LogFileName As String = Path.Combine(Application.StartupPath, "DartGame.log")
+    Private ReadOnly RandomGen As New Random()
+    Private Const DartRadius As Integer = 6
+
+    ' --- State ---
+    Private currentRound As Integer = 0
+    Private currentThrowCount As Integer = 0
+    Private currentRoundDarts As New List(Of Point)()
+    Private isPlayMode As Boolean = False
+    Private isReviewMode As Boolean = False
+
+    ' ---------------------------
+    ' FORM LOAD
+    ' ---------------------------
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' ensure form receives key presses (spacebar)
+        Me.KeyPreview = True
+
+        ModeLabel.Text = "Mode: Idle"
+        RoundNumberLabel.Text = "Round: 0"
+        ThrowCountLabel.Text = $"Throw: 0 / {NumThrowsPerRound}"
+
+        InitializeDartboardImage()
+        LoadRoundsIntoListBox()
+
+        ' Tooltips (optional; create MainToolTip in designer)
+        Try
+            MainToolTip.SetToolTip(DartboardPictureBox, "This is the dartboard!")
+            MainToolTip.SetToolTip(OpenLogButton, "Click to open the log file in Notepad.")
+            MainToolTip.SetToolTip(StartRoundButton, "Start a new round.")
+            MainToolTip.SetToolTip(ReviewButton, "Enter review mode to inspect saved rounds.")
+            MainToolTip.SetToolTip(QuitButton, "Quit the game.")
+            MainToolTip.SetToolTip(RoundsListBox, "Select a logged round to display its darts.")
+        Catch
+            ' ignore if tooltip control not present
+        End Try
+
+        EnterIdleMode()
+    End Sub
+
+    ' ---------------------------
+    ' INITIALIZE / CLEAR DARTBOARD
+    ' ---------------------------
+    Private Sub InitializeDartboardImage()
+        Dim bmp As New Bitmap(Math.Max(1, DartboardPictureBox.Width), Math.Max(1, DartboardPictureBox.Height))
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.Clear(Color.White)
+        End Using
+        DartboardPictureBox.Image = bmp
+    End Sub
+
+    Private Sub ClearDartboard()
+        If DartboardPictureBox.Image Is Nothing OrElse
+           DartboardPictureBox.Image.Width <> DartboardPictureBox.Width OrElse
+           DartboardPictureBox.Image.Height <> DartboardPictureBox.Height Then
+            InitializeDartboardImage()
+            Return
+        End If
+
+        Using g As Graphics = Graphics.FromImage(DartboardPictureBox.Image)
+            g.Clear(Color.White)
+        End Using
+        DartboardPictureBox.Invalidate()
+    End Sub
+
+    ' ---------------------------
+    ' START / NEW ROUND
+    ' ---------------------------
+    Private Sub StartRoundButton_Click(sender As Object, e As EventArgs) Handles StartRoundButton.Click
+        If isReviewMode Then ExitReviewMode()
+        StartNewRound()
+    End Sub
+
+    Private Sub StartNewRound()
+        RoundsListBox.Items.Clear()
+
+        isPlayMode = True
+        isReviewMode = False
+        currentRound += 1
+        currentThrowCount = 0
+        currentRoundDarts.Clear()
+
+        ModeLabel.Text = "Mode: Play"
+        RoundNumberLabel.Text = $"Round: {currentRound}"
+        ThrowCountLabel.Text = $"Throw: {currentThrowCount} / {NumThrowsPerRound}"
+
+        ClearDartboard()
+    End Sub
+
+    ' ---------------------------
+    ' KEYBOARD (SPACE THROWS DART)
+    ' ---------------------------
+    Protected Overrides Sub OnKeyDown(e As KeyEventArgs)
+        MyBase.OnKeyDown(e)
+        If e.KeyCode = Keys.Space Then
+            e.Handled = True
+            e.SuppressKeyPress = True
+            If isPlayMode Then
+                PerformThrow()
+            End If
+        End If
+    End Sub
+
+    ' ---------------------------
+    ' THROW DART
+    ' ---------------------------
+    Private Sub PerformThrow()
+        ' Only allow throws in play mode
+        If Not isPlayMode Then Return
+
+        ' Generate point and add to current round
+        Dim pt As Point = GenerateRandomPointInsideDartboard(DartRadius)
+        currentRoundDarts.Add(pt)
+        currentThrowCount += 1
+
+        ' Update UI and draw
+        ThrowCountLabel.Text = $"Throw: {currentThrowCount} / {NumThrowsPerRound}"
+        DrawDart(pt)
+
+        ' If round complete, log and stop play mode
+        If currentThrowCount >= NumThrowsPerRound Then
+            Try
+                LogRound(currentRound, currentRoundDarts)
+                MessageBox.Show($"Round {currentRound} saved to log.", "Round Logged", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show($"Error logging round: {ex.Message}", "File I/O Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            ' reload listbox so review shows latest
+            LoadRoundsIntoListBox()
+
+            ' finish round
+            isPlayMode = False
+            ModeLabel.Text = "Mode: Idle"
+        End If
+    End Sub
+
+    Private Function GenerateRandomPointInsideDartboard(radius As Integer) As Point
+        Dim bmpW = Math.Max(1, DartboardPictureBox.Width)
+        Dim bmpH = Math.Max(1, DartboardPictureBox.Height)
+
+        Dim minX As Integer = radius
+        Dim maxX As Integer = Math.Max(radius, bmpW - radius - 1)
+        Dim minY As Integer = radius
+        Dim maxY As Integer = Math.Max(radius, bmpH - radius - 1)
+
+        Dim x = RandomGen.Next(minX, maxX + 1)
+        Dim y = RandomGen.Next(minY, maxY + 1)
+
+        Return New Point(x, y)
+    End Function
+
+    Private Sub DrawDart(p As Point)
+        If DartboardPictureBox.Image Is Nothing Then InitializeDartboardImage()
+
+        Dim bmp As Bitmap = DirectCast(DartboardPictureBox.Image, Bitmap)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.FillEllipse(Brushes.Red, p.X - DartRadius, p.Y - DartRadius, DartRadius * 2, DartRadius * 2)
+            g.DrawLine(Pens.Black, p.X - 3, p.Y, p.X + 3, p.Y)
+            g.DrawLine(Pens.Black, p.X, p.Y - 3, p.X, p.Y + 3)
+        End Using
+        DartboardPictureBox.Invalidate()
+    End Sub
+
+    ' ---------------------------
+    ' FILE I/O: LOG ROUND (with timestamp)
+    ' Format: Round|<round>|yyyy-MM-dd HH:mm:ss|x1,y1;x2,y2;x3,y3
+    ' ---------------------------
+    Private Sub LogRound(roundNumber As Integer, darts As List(Of Point))
+        If darts Is Nothing OrElse darts.Count <> NumThrowsPerRound Then
+            Throw New InvalidOperationException("Attempt to log incomplete round.")
+        End If
+
+        Dim timestamp As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+        Dim coords As String = String.Join(";", darts.Select(Function(p) $"{p.X},{p.Y}"))
+        Dim line As String = $"Round|{roundNumber}|{timestamp}|{coords}"
+
+        Try
+            Using sw As New StreamWriter(LogFileName, True)
+                sw.WriteLine(line)
+            End Using
+        Catch ex As Exception
+            Throw ' let caller display message
+        End Try
+    End Sub
+
+    ' ---------------------------
+    ' LOAD ROUNDS INTO LISTBOX (clears first)
+    ' ---------------------------
+    Private Sub LoadRoundsIntoListBox()
+        RoundsListBox.Items.Clear()
+
+        If Not File.Exists(LogFileName) Then
+            Return
+        End If
+
+        Try
+            Dim lines = File.ReadAllLines(LogFileName)
+            For Each ln As String In lines
+                If String.IsNullOrWhiteSpace(ln) Then Continue For
+                ' Add the raw stored line (it's readable) or you could format it for display
+                RoundsListBox.Items.Add(ln)
+            Next
+        Catch ex As Exception
+            MessageBox.Show($"Error reading log file: {ex.Message}", "File I/O Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' ---------------------------
+    ' REVIEW MODE
+    ' ---------------------------
+    Private Sub ReviewButton_Click(sender As Object, e As EventArgs) Handles ReviewButton.Click
+        If isPlayMode Then
+            ' finish/stop play mode before entering review
+            isPlayMode = False
+        End If
+
+        EnterReviewMode()
+    End Sub
+
+    Private Sub EnterReviewMode()
+        isReviewMode = True
+        isPlayMode = False
+        ModeLabel.Text = "Mode: Review"
+        ThrowCountLabel.Text = $"Throw: 0 / {NumThrowsPerRound}"
+        ClearDartboard()
+        LoadRoundsIntoListBox()
+    End Sub
+
+    Private Sub ExitReviewMode()
+        isReviewMode = False
+        ModeLabel.Text = "Mode: Idle"
+        ClearDartboard()
+    End Sub
+
+    ' When user selects a saved round in the ListBox, draw it
+    Private Sub RoundsListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles RoundsListBox.SelectedIndexChanged
+        If RoundsListBox.SelectedIndex = -1 Then Return
+        If Not isReviewMode Then Return
+
+        Dim line As String = RoundsListBox.SelectedItem.ToString()
+        Dim pts As List(Of Point) = ParseRoundLine(line)
+        If pts Is Nothing OrElse pts.Count <> NumThrowsPerRound Then
+            MessageBox.Show("Selected round is invalid or corrupted.", "Parse Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ClearDartboard()
+        For Each p In pts
+            DrawDart(p)
+        Next
+    End Sub
+
+    Private Function ParseRoundLine(line As String) As List(Of Point)
+        ' Expected format: Round|<round>|yyyy-MM-dd HH:mm:ss|x1,y1;x2,y2;x3,y3
+        Try
+            Dim parts = line.Split("|"c)
+            If parts.Length < 4 Then Return Nothing
+            Dim coordsPart = parts(3)
+            Dim pairs = coordsPart.Split(";"c)
+            Dim pts As New List(Of Point)
+            For Each pair In pairs
+                Dim xy = pair.Split(","c)
+                If xy.Length <> 2 Then Return Nothing
+                Dim x = Integer.Parse(xy(0).Trim())
+                Dim y = Integer.Parse(xy(1).Trim())
+                ' Clip to ensure within bounds
+                x = Math.Max(DartRadius, Math.Min(DartboardPictureBox.Width - DartRadius - 1, x))
+                y = Math.Max(DartRadius, Math.Min(DartboardPictureBox.Height - DartRadius - 1, y))
+                pts.Add(New Point(x, y))
+            Next
+            Return pts
+        Catch
+            Return Nothing
+        End Try
+    End Function
+
+    ' ---------------------------
+    ' OPEN LOG IN NOTEPAD
+    ' ---------------------------
+    Private Sub OpenLogButton_Click(sender As Object, e As EventArgs) Handles OpenLogButton.Click
+        If Not File.Exists(LogFileName) Then
+            MessageBox.Show("No log file yet! Play a round first.", "Log Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        Try
+            Process.Start("notepad.exe", LogFileName)
+        Catch ex As Exception
+            MessageBox.Show($"Failed to open Notepad: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' ---------------------------
+    ' QUIT GAME
+    ' ---------------------------
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Me.Close()
+    End Sub
+
+    ' ---------------------------
+    ' HELPER: IDLE MODE
+    ' ---------------------------
+    Private Sub EnterIdleMode()
+        isPlayMode = False
+        isReviewMode = False
+        ModeLabel.Text = "Mode: Idle"
+        ThrowCountLabel.Text = $"Throw: 0 / {NumThrowsPerRound}"
+        currentRoundDarts.Clear()
+    End Sub
+
+End Class
+
+
